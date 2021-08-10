5ab12125f67c91988e5cb608c426eb8c2d1b4e51
Matt <carlotta.matt@gmail.com>
2021-08-09 19:21:55 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::json::{json, Value};
+use rocket::serde::{Deserialize, Serialize};
+
+// #[cfg(test)]
+// mod tests;
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    message: String,
+}
+
+#[get("/")]
+fn index() -> Value {
+    json!({ "message": "Welcome to the Rust API!" })
+}
+
+#[get("/hello")]
+fn hello() -> Value {
+    json!({ "message": "Hello, world!" })
+}
+
+#[get("/world")]
+fn world() -> Value {
+    json!({ "message": "Goodbye, world!" })
+}
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!({
+        "status": 404,
+        "reason": "Resource was not found."
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, hello, world])
+        .register("/", catchers![not_found])
+}
