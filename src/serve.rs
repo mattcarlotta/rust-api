8449298b5e4adf876bf9b038ecba9221df738602
Matt <carlotta.matt@gmail.com>
2021-08-14 16:51:25 -0700

    Update serve fn with @togglebyte code review

--- src/serve.rs
+++ src/serve.rs
@@ -1,7 +1,7 @@
 // #![allow(dead_code, unused_variables)]
 
 use crate::lrucache::LRUCache;
-use crate::utils::{get_extension_from_filename, get_file_path, get_string_path};
+use crate::utils::{get_file_path, get_root_dir, get_string_path};
 use futures_locks::Mutex;
 use image::imageops::FilterType;
 use regex::Regex;
@@ -10,16 +10,18 @@ use rocket::fs::{relative, FileServer};
 use rocket::http::ContentType;
 use rocket::response::content::Custom;
 use rocket::State;
+use std::ffi::OsStr;
 use std::path::PathBuf;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
 
 type Cache = Mutex<LRUCache<String, Vec<u8>>>;
 
-type CustomResponseVector = Custom<Vec<u8>>;
+type ResponseVec = Custom<Vec<u8>>;
 
 // API endpoint: GET http://domain.com/image/example.png?width=800
 // Try visiting:
+//   http://127.0.0.1:5000/placeholder <-- is this possible?
 //   http://127.0.0.1:5000/placeholder.png
 //   http://127.0.0.1:5000/placeholder.png?width=800
 //   http://127.0.0.1:5000/placeholder.png?width=1000
@@ -29,69 +31,79 @@ async fn serve_image(
   path: PathBuf,
   width: Option<&str>,
   state: &State<Cache>,
-) -> Option<CustomResponseVector> {
-  // convert path to string -> <filename>.<ext>
-  let fp = get_string_path(path.to_path_buf());
+) -> Option<ResponseVec> {
+  // Ensure that this is a directory
+  // so that it's not possible to call http://127.0.0.1:5000/placeholder
+  // otherwise, fallback to 404 route
+  if path.extension().is_none() || path.as_os_str().is_empty() {
+    rocket::info_!("The file path is invalid.");
+    return None;
+  }
 
-  // if present, strip any included "_<width>" from the filename
-  let filename = Regex::new(r"_.*[\d]")
-    .unwrap()
-    .replace_all(&fp, "")
-    .to_string();
+  // Here we def have a <file>.<ext>
+  // convert path to string -> <filename>.<ext>
+  let (filename, content_type) = {
+    // if present, strip any included "_<width>" from the filename
+    let filename = Regex::new(r"_.*[\d]")
+      .unwrap()
+      .replace_all(&get_string_path(path.to_path_buf()), "")
+      .to_string();
+    // derive content type from filename -> example.png -> .png -> ContentType::PNG
+    let content_type = path
+      .extension()
+      .and_then(OsStr::to_str)
+      .and_then(ContentType::from_extension);
+
+    if content_type.is_none() {
+      rocket::info_!("The content type is missing.");
+    }
+
+    (filename, content_type?)
+  };
+
+  // TODO - Create standardized widths to prevent unlimited amount of image resizes
+  // converts supplied "width" argument to a valid u32
+  let parsed_width = width
+    .map(str::parse::<u32>)
+    .map(Result::ok)
+    .flatten()
+    .unwrap_or(0);
 
   // retrieve static folder + filename -> /path/to/static/<filename> buffer
   let fn_path = get_file_path(filename);
 
-  // derive content type from filename -> example.png -> .png -> ContentType::PNG
-  let content_type = match get_extension_from_filename(&fp) {
-    Some(ext) => ContentType::from_extension(ext),
-    None => None,
-  };
-
-  // fallback to 404 route if path is empty or file extension is invalid
-  if path.as_os_str().is_empty() || content_type.is_none() {
-    rocket::info_!("The file path and/or content type are invalid.");
-    return None;
-  }
-  let mut cache = state.lock().await;
-
-  // retrieve string of static folder with filename -> /path/to/static/<filename>.<ext>
-  let pathname = get_string_path(fn_path.to_path_buf());
-
-  // TODO - Create standardized widths to prevent unlimited amount of image resizes
-  // converts supplied "width" argument to a valid u32
-  let parsed_width = width.unwrap_or("0").parse::<u32>().unwrap_or(0);
-
-  // store original pathname if no width query or store <filename>_<width>.<ext>
+  // store original pathname if no width query: <rootdir>/<filename>_<width>.<ext>
+  // or store pathname with width: <rootdir>/<filename>_<width>.<ext>
   let requested_fp = match parsed_width == 0 {
-    true => pathname,
+    true => get_string_path(&fn_path),
     false => {
-      let image_path = get_string_path(fn_path.to_path_buf());
+      let image_path = get_string_path(&fn_path);
 
-      let new_image_path: Vec<&str> = image_path.split('.').collect();
+      let ext = fn_path
+        .extension()
+        .and_then(OsStr::to_str)
+        .expect(&format!("File is missing extension: {}", image_path));
 
-      format!(
-        "{}_{}.{}",
-        &new_image_path[0],
-        &parsed_width.to_string(),
-        &new_image_path[1],
-      )
+      let stem = fn_path
+        .file_stem()
+        .and_then(OsStr::to_str)
+        .expect(&format!("File is missing extension: {}", image_path));
+
+      format!("{}/{}_{}.{}", get_root_dir(), stem, parsed_width, ext)
     }
   };
-
+  let mut cache = state.lock().await;
   // determine if cache contains requested image
   match cache.contains_key(&requested_fp) {
     true => {
       rocket::info_!("Served requested image from cache.");
-
       // retrieve image from the cache
-      let stored_entry = match cache.get(&requested_fp.to_string()) {
-        Some(val) => val,
-        None => panic!("Unable to retrieve image entry from cache."),
-      };
+      let stored_entry = cache
+        .get(&requested_fp)
+        .expect("Unable to retrieve image entry from cache.");
 
       // respond to request with cached image
-      Some(Custom(content_type.unwrap(), stored_entry.to_vec()))
+      Some(Custom(content_type, stored_entry.to_vec()))
     }
     false => {
       // return if requested image doesn't exist
@@ -101,7 +113,7 @@ async fn serve_image(
 
       // TODO - Make sure requested image size doesn't extend beyond actual image dimensions
       // create a new image from original if one doesn't exist
-      if !get_file_path(requested_fp.to_string()).is_file() {
+      if !get_file_path(&requested_fp).is_file() {
         let current_image = image::open(fn_path).expect("Failed to open image.");
         current_image
           .resize(parsed_width, parsed_width, FilterType::CatmullRom)
@@ -112,32 +124,163 @@ async fn serve_image(
       // open requested image
       let mut named_file = match File::open(&requested_fp).await {
         Ok(file) => file,
-        Err(_err) => panic!("Unable to open image."),
+        Err(reason) => {
+          rocket::info_!("Unable to open image: {}", reason);
+          return None;
+        }
       };
 
       // read the contents of the image
       let mut contents = Vec::new();
       match named_file.read_to_end(&mut contents).await {
         Ok(vec) => vec,
-        Err(_f) => panic!("Unable to read the contents of the image."),
+        Err(reason) => {
+          rocket::info_!("Unable to read the contents of the image: {}", reason);
+          return None;
+        }
       };
 
       // store read contents into cache
-      cache.insert(requested_fp.to_string(), contents);
+      cache.insert(requested_fp.clone(), contents);
 
       // retrieve contents from the cache
-      let stored_entry = match cache.get(&requested_fp.to_string()) {
+      let stored_entry = match cache.get(&requested_fp) {
         Some(val) => val,
-        None => panic!("Unable to retrieve entry from cache."),
+        None => {
+          rocket::info_!("Unable to retrieve entry from cache.");
+          return None;
+        }
       };
 
       rocket::info_!("Saved requested image into cache.");
 
       // respond to request with (original/resized) image
-      Some(Custom(content_type.unwrap(), stored_entry.to_vec()))
+      Some(Custom(content_type, stored_entry.clone()))
     }
   }
 }
+
+// OLD:
+// API endpoint: GET http://domain.com/image/example.png?width=800
+// Try visiting:
+//   http://127.0.0.1:5000/placeholder.png
+//   http://127.0.0.1:5000/placeholder.png?width=800
+//   http://127.0.0.1:5000/placeholder.png?width=1000
+//   http://127.0.0.1:5000/placeholder_500.png?width=1000 => falls back to placeholder.png@1000px
+// #[get("/image/<path..>?<width>")]
+// async fn serve_image(
+//   path: PathBuf,
+//   width: Option<&str>,
+//   state: &State<Cache>,
+// ) -> Option<CustomResponseVector> {
+//   // convert path to string -> <filename>.<ext>
+//   let fp = get_string_path(path.to_path_buf());
+
+//   // if present, strip any included "_<width>" from the filename
+//   let filename = Regex::new(r"_.*[\d]")
+//     .unwrap()
+//     .replace_all(&fp, "")
+//     .to_string();
+
+//   // retrieve static folder + filename -> /path/to/static/<filename> buffer
+//   let fn_path = get_file_path(filename);
+
+//   // derive content type from filename -> example.png -> .png -> ContentType::PNG
+//   let content_type = match get_extension_from_filename(&fp) {
+//     Some(ext) => ContentType::from_extension(ext),
+//     None => None,
+//   };
+
+//   // fallback to 404 route if path is empty or file extension is invalid
+//   if path.as_os_str().is_empty() || content_type.is_none() {
+//     rocket::info_!("The file path and/or content type are invalid.");
+//     return None;
+//   }
+//   let mut cache = state.lock().await;
+
+//   // retrieve string of static folder with filename -> /path/to/static/<filename>.<ext>
+//   let pathname = get_string_path(fn_path.to_path_buf());
+
+//   // TODO - Create standardized widths to prevent unlimited amount of image resizes
+//   // converts supplied "width" argument to a valid u32
+//   let parsed_width = width.unwrap_or("0").parse::<u32>().unwrap_or(0);
+
+//   // store original pathname if no width query or store <filename>_<width>.<ext>
+//   let requested_fp = match parsed_width == 0 {
+//     true => pathname,
+//     false => {
+//       let image_path = get_string_path(fn_path.to_path_buf());
+
+//       let new_image_path: Vec<&str> = image_path.split('.').collect();
+
+//       format!(
+//         "{}_{}.{}",
+//         &new_image_path[0],
+//         &parsed_width.to_string(),
+//         &new_image_path[1],
+//       )
+//     }
+//   };
+
+//   // determine if cache contains requested image
+//   match cache.contains_key(&requested_fp) {
+//     true => {
+//       rocket::info_!("Served requested image from cache.");
+
+//       // retrieve image from the cache
+//       let stored_entry = match cache.get(&requested_fp.to_string()) {
+//         Some(val) => val,
+//         None => panic!("Unable to retrieve image entry from cache."),
+//       };
+
+//       // respond to request with cached image
+//       Some(Custom(content_type.unwrap(), stored_entry.to_vec()))
+//     }
+//     false => {
+//       // return if requested image doesn't exist
+//       if !fn_path.is_file() {
+//         return None;
+//       }
+
+//       // TODO - Make sure requested image size doesn't extend beyond actual image dimensions
+//       // create a new image from original if one doesn't exist
+//       if !get_file_path(requested_fp.to_string()).is_file() {
+//         let current_image = image::open(fn_path).expect("Failed to open image.");
+//         current_image
+//           .resize(parsed_width, parsed_width, FilterType::CatmullRom)
+//           .save(&requested_fp)
+//           .expect("Failed to resize image.");
+//       }
+
+//       // open requested image
+//       let mut named_file = match File::open(&requested_fp).await {
+//         Ok(file) => file,
+//         Err(_err) => panic!("Unable to open image."),
+//       };
+
+//       // read the contents of the image
+//       let mut contents = Vec::new();
+//       match named_file.read_to_end(&mut contents).await {
+//         Ok(vec) => vec,
+//         Err(_f) => panic!("Unable to read the contents of the image."),
+//       };
+
+//       // store read contents into cache
+//       cache.insert(requested_fp.to_string(), contents);
+
+//       // retrieve contents from the cache
+//       let stored_entry = match cache.get(&requested_fp.to_string()) {
+//         Some(val) => val,
+//         None => panic!("Unable to retrieve entry from cache."),
+//       };
+
+//       rocket::info_!("Saved requested image into cache.");
+
+//       // respond to request with (original/resized) image
+//       Some(Custom(content_type.unwrap(), stored_entry.to_vec()))
+//     }
+//   }
+// }
 
 pub fn stage() -> AdHoc {
   AdHoc::on_ignite("serve", |rocket| async {
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,4 @@
 use rocket::fs::relative;
-use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
 /// Converts a string into a path buffer.
@@ -9,9 +8,27 @@ use std::path::{Path, PathBuf};
 /// * `path` - String
 ///
 /// Usage: ```get_file_path(path);```
-pub fn get_file_path(path: String) -> PathBuf {
-  Path::new(relative!("static")).join(path.clone())
+/// notes: using AsRef creates a cheap reference-to-reference conversion
+pub fn get_root_dir() -> &'static str {
+  Path::new(relative!("static")).to_str().unwrap()
 }
+
+/// Converts a string into a path buffer.
+///
+/// Arguments:
+///
+/// * `path` - String
+///
+/// Usage: ```get_file_path(path);```
+/// notes: using AsRef creates a cheap reference-to-reference conversion
+pub fn get_file_path(path: impl AsRef<Path>) -> PathBuf {
+  Path::new(relative!("static")).join(path)
+}
+
+// OLD:
+// pub fn get_file_path(path: String) -> PathBuf {
+//   Path::new(relative!("static")).join(path.clone())
+// }
 
 /// Converts a path buffer into a string.
 ///
@@ -20,20 +37,29 @@ pub fn get_file_path(path: String) -> PathBuf {
 /// * `path` - PathBuf
 ///
 /// Usage: ```get_string_path(path);```
-pub fn get_string_path(path: PathBuf) -> String {
-  path.into_os_string().into_string().unwrap()
+/// notes: using AsRef creates a cheap reference-to-reference conversion
+/// to_str: yields a &str slice if the OsStr is valid Unicode.
+/// into: value-to-value conversion that consumes the input value
+pub fn get_string_path(path: impl AsRef<Path>) -> String {
+  path.as_ref().to_str().unwrap().into()
 }
 
-/// Retrieves a file extension from a string.
-///
-/// Arguments:
-///
-/// * `filename` - &str
-///
-/// Usage: ```get_extension_from_filename(&filename);```
-pub fn get_extension_from_filename(filename: &str) -> Option<&str> {
-  match Path::new(filename).extension().and_then(OsStr::to_str) {
-    Some(ext) => Some(ext),
-    None => None,
-  }
-}
+// OLD:
+// pub fn get_string_path(path: PathBuf) -> String {
+//   path.into_os_string().into_string().unwrap()
+// }
+
+// Retrieves a file extension from a string.
+//
+// Arguments:
+//
+// * `filename` - &str
+//
+// Usage: get_extension_from_filename(&filename);
+//
+// pub fn get_extension_from_filename(filename: &str) -> Option<&str> {
+//   match Path::new(filename).extension().and_then(OsStr::to_str) {
+//     Some(ext) => Some(ext),
+//     None => None,
+//   }
+// }
